10954d62b23d9824d519b06dd9985f1ef8e2406e
icafeba <155060608+icafeba@users.noreply.github.com>
2023-12-28 22:50:21 +0800

    Update nbtv.php

--- api/nbtv.php
+++ api/nbtv.php
@@ -1,14 +1,34 @@
 <?php
-$id = isset($_GET['id'])?$_GET['id']:'nbtv1';//nbtv1,2,3,4,5,6
-$n = json_encode([url => 'http://liveplay.nbtv.cn/live/'.$id.'_md.m3u8',playType => 'live',type => 'cdn',cdnEncrypt => 'd058c6c09b8cec3e4c8391557ac977714a35da41c4cfd40c75d6b4fdb37750b40af99e78071b72269b1614077c887c9431ce02c56739ed3a878ac3445c6352497f6ab0dec816df39192412e95509d2df4808e102380dd64ae67105a7266ec8ed580998e4e34dd62002039f872e1bda820ec4d9eaf8a11d658155d26c74125323c71e9743653e192327f3b6944ef0d219250f53718c6c38512eb9f142afe25f0838dff439d47fa695cb0eaf6473e4b4b6be62bfcbd240bc8d77d250809c1796c3cc54bdc2b70740c58cb3e39cf0ca4472d7c04c433a1daa8c6853e887aa36046c5bb959a58c0df05b81b399fad91372fea0aae029b73101c15d4bf220bbf975f7cb0a0c7ba42817d4aeebc8b8b6a3f2e83760724205a1f0eeab3dc2501d520baeab6463a0189135c00c96896e000fc28c',cdnIndex => 0,]);
-$ch = curl_init('http://em.chinamcloud.com/player/encryptUrl');
+$id = $_GET['id'];//id=1 2 3 4 5
+$d = json_decode(file_get_contents('http://media.cloud.nbtv.cn:84/nbtv/media/playerJson/liveChannel/8b2663864ab04a0799acd60e4b91f677_PlayerParamProfile.json'));
+$cdn=( $d->paramsConfig->cdnConfigEncrypt);
+$post_url = "http://em.chinamcloud.com/player/encryptUrl";
+$postdata='{"url":"http://liveplay.nbtv.cn/live/nbtv'.$id.'_md.m3u8","playType":"live","type":"cdn","cdnEncrypt":"'.$cdn.'","cdnIndex":0}';
+$headerArr= array(
+'Accept-Language: zh-CN,zh;q=0.9',
+'Connection: keep-alive',
+'Content-Length: 720',
+'Content-Type: application/json;charset=UTF-8',
+'Host: em.chinamcloud.com',
+'Origin: http://www.nbtv.cn',
+'Referer: http://www.nbtv.cn/gbds/folder8458/NBTV1/index.shtml',
+);
+$UserAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+              //Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $post_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_ENCODING, " ");
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArr);
+curl_setopt($ch, CURLOPT_USERAGENT, $UserAgent);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $n);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-$result = curl_exec($ch);
-curl_close($ch);
-$playurl = json_decode($result)->url;
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+$content = curl_exec($ch);
+curl_close($ch); // 关闭CURL会话
+$json=json_decode($content);
+$playurl =$json->url;
+echo $playurl;
 header('Location:'.$playurl);
-//echo $playurl;
 ?>
